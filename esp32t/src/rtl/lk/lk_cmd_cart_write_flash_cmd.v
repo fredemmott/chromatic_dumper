23ade477746fdd6f9357315a69fc3add36d82f3b
Fred Emmott <git@fredemmott.com>
2026-05-27 16:21:37 -0500

    some off-by-one mistakes in cart_write_flash_cmd
    Includes some refactoring too as this significantly slowed things down

--- esp32t/src/rtl/lk/lk_cmd_cart_write_flash_cmd.v
+++ esp32t/src/rtl/lk/lk_cmd_cart_write_flash_cmd.v
@@ -1,6 +1,6 @@
 import lk_types::*;
 
-module lk_cmd_cart_write_flash_cmd_t(
+module lk_cmd_cart_write_flash_cmd_t (
     input  wire        clk,
     input  wire        enable,
     output reg         complete,
@@ -30,7 +30,7 @@ state_t state_next;
 always @(*) begin
     state_next = state;
     unique case(state)
-        S_RX: if (idx == 7) state_next = S_EXEC;
+        S_RX: if (idx >= 6) state_next = S_EXEC;
         S_EXEC: state_next = S_WAIT;
         S_WAIT: if (cart_complete) state_next = S_COMPLETE;
         default: ;
@@ -47,17 +47,20 @@ always @(posedge clk) begin
     end
 end
 
+// byte [0]:    is flash cart (unused)
+//      [1..4]: address (2 MSB for AGB only)
+//      [5..6]: data (MSB for AGB only)
+// ... so we only need the last 4 rx bytes
+reg [7:0] rx_buf [0:3];
+assign cart_req_address = { rx_buf[0], rx_buf[1] };
+assign cart_req_data = rx_buf[3];
+integer i;
 always @(posedge clk) begin
     if (rx_valid) begin
-        // byte [0]:    is flash cart (unused)
-        //      [1..5]: address (2 MSB for AGB only)
-        //      [6..7]: data (MSB for AGB only)
-        unique case(idx)
-            4: cart_req_address[15:8] <= rx_data;
-            5: cart_req_address[7:0] <= rx_data;
-            6: cart_req_data <= rx_data;
-            default: ;
-        endcase
+        for (i = 0; i < 3; i = i + 1) begin
+            rx_buf[i] <= rx_buf[i + 1];
+        end
+        rx_buf[3] <= rx_data;
     end
 end
 
