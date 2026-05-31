ca1361ce4f6753b91aa4877d5f5a88a9a75fcab6
Fred Emmott <git@fredemmott.com>
2026-05-30 19:09:38 -0500

    Correctly-completing CART_WRITE_FLASH_CMD

--- esp32t/src/rtl/lk/lk_cmd_cart_write_flash_cmd.v
+++ esp32t/src/rtl/lk/lk_cmd_cart_write_flash_cmd.v
@@ -19,11 +19,9 @@ always @(posedge clk) begin
     rx_data_r <= rx_data;
 end
 
-reg [2:0] idx;
-
 // As usual, the packet is for 32-bit address and 16-bit data (AGB), but we only support
 // DMG which is 16-bit addresses and 8-bit data
-logic [7:0] commands_remaining;
+logic [7:0] commands_to_rx;
 logic [7:0] commands_pending;
 
 typedef enum {
@@ -36,6 +34,7 @@ typedef enum {
 state_t state;
 always @(posedge clk) complete <= (state == S_COMPLETE);
 
+
 // byte 0: 'is flashcart' (unused)
 //      1: number of commands
 //      ...: commands
@@ -43,69 +42,67 @@ always @(posedge clk) complete <= (state == S_COMPLETE);
 // command bytes [0..3]: address
 //               [4..5]: data (command)
 
-wire this_command_received = (idx >= 3'd5);
+reg [2:0] idx;
+wire rx_command_byte = (state == S_RX_COMMAND) && rx_valid_r;
 
-reg [2:0] next_idx;
-always @(*) begin
-    next_idx = idx;
+always @(posedge clk) begin
+    cart_req_valid <= 1'b0;
     if (rx_valid_r) begin
-        next_idx = idx + 1'd1;
-        unique case (state)
-            S_RX_HEADER: if (idx[0]) next_idx = 0; // 1 -> S_RX_COMMAND
-            S_RX_COMMAND: if (this_command_received) next_idx = 0; // next command
+        unique case (idx)
+            2: cart_req_address[15:8] <= rx_data_r;
+            3: cart_req_address[7:0] <= rx_data_r;
+            5: begin
+                cart_req_valid <= 1'b1;
+                cart_req_data <= rx_data_r;
+            end
             default: ;
         endcase
     end
 end
-always @(posedge clk) idx <= enable ? next_idx : 0;
 
-wire all_commands_received = this_command_received && !commands_remaining;
-wire all_commands_complete = (commands_pending == 0);
-
-state_t next_state;
-always @(*) begin
-    next_state = state;
-    unique case(state)
-        S_RX_HEADER: if (idx[0]) next_state = S_RX_COMMAND;
-        S_RX_COMMAND: if (all_commands_received) next_state = S_WAIT;
-        S_WAIT: if (all_commands_complete) next_state = S_COMPLETE;
-        // S_COMPLETE: /* terminal until !enable */ ;
-        default: ;
-    endcase
-end
-always @(posedge clk) state <= enable ? next_state : S_RX_HEADER;
-
+wire rx_command_count = (state == S_RX_HEADER) && rx_valid_r && idx[0];
 always @(posedge clk) begin
-    if (state == S_RX_HEADER) commands_pending <= rx_data_r;
-    else if (cart_complete) commands_pending <= commands_pending - 1'd1;
-end
-always @(posedge clk) begin
-    if (state == S_RX_HEADER) begin
-        commands_remaining <= rx_data_r;
+    if (rx_command_count) begin
+        commands_to_rx <= rx_data_r;
+        commands_pending <= rx_data_r;
+    end else if (rx_command_byte) begin
+        if (idx == 0) commands_to_rx <= commands_to_rx - 1'd1;
     end
-    else if ((state == S_RX_COMMAND) && idx == 0) commands_remaining <= commands_remaining - 1'd1;
+
+    if (cart_complete) commands_pending <= commands_pending - 1'd1;
 end
 
-wire rx_command = rx_valid_r && (state == S_RX_COMMAND);
+wire rx_command_complete = rx_valid_r && (idx == 5);
+
+// rx_command_count could also be called 'rx_header_complete'
+wire reset_idx = rx_command_count || rx_command_complete || !enable;
 
 always @(posedge clk) begin
-    cart_req_valid <= 1'b0;
+    if (reset_idx) begin
+        idx <= 1'd0;
+    end else if (rx_valid_r) begin
+        idx <= idx + 1'd1;
+    end
+end
+
+wire rx_final_command = rx_command_complete && !commands_to_rx;
+
+always @(posedge clk) begin
     if (!enable) begin
-        cart_req_address <= '0;
-        cart_req_data <= '0;
+        state <= S_RX_HEADER;
     end else begin
-        if (rx_command) begin
-            unique case (idx)
-                2: cart_req_address[15:8] <= rx_data_r;
-                3: cart_req_address[7:0] <= rx_data_r;
-                5: begin
-                    cart_req_data <= rx_data_r;
-                    cart_req_valid <= 1'b1;
+        unique case(state)
+            S_RX_HEADER: begin
+                if (rx_command_count) begin
+                    state <= (|rx_data_r) ? S_RX_COMMAND : S_COMPLETE;
                 end
-                default: ; // others are MSB data for AGB only
-            endcase
-        end
+            end
+            S_RX_COMMAND: if (rx_final_command) state <= S_WAIT;
+            S_WAIT: if (!commands_pending) state <= S_COMPLETE;
+            default: ;
+        endcase
     end
 end
+
 
 endmodule
