9c84e6a56aa578d43d595d5606f3a1bafb558a74
Fred Emmott <git@fredemmott.com>
2026-06-02 21:55:18 -0500

    ⚰️ add unused lk_cmd_flash_program.v
    Took a lot of work and fighting gowin to get this. Unused in the end,
    but let's preserve it for future reference

--- esp32t/src/rtl/lk/lk_cmd_flash_program.v
+++ esp32t/src/rtl/lk/lk_cmd_flash_program.v
@@ -49,28 +49,38 @@ end
 // byte [0..3]: address (only lower 16 bits for us on DMG)
 //      [4..5]: data    (only lower 8 bits)
 //
-reg [2:0] SFC_cmd_idx;
-reg [2:0] SFC_byte_idx;
+reg [5:0] SFC_byte_sr;
 
 typedef enum {
     SFC_RX_COMMAND_SET,
     SFC_RX_FLASH_METHOD,
-    SFC_RX_COMMAND,
+    SFC_RX_FLASH_WE_PIN, // TODO? shouldn't this be done by SET_VARIABLE?
+    SFC_RX_COMMAND_0,
+    SFC_RX_COMMAND_1,
+    SFC_RX_COMMAND_2,
+    SFC_RX_COMMAND_3,
+    SFC_RX_COMMAND_4,
+    SFC_RX_COMMAND_5,
     SFC_COMPLETE
 } SFC_state_t;
 SFC_state_t SFC_state;
 always @(posedge clk) SET_FLASH_CMD_complete <= SFC_state == SFC_COMPLETE;
 
-wire SFC_rx_this_command_complete = rx_valid_r && (SFC_byte_idx == 5);
-wire SFC_rx_all_commands_complete = SFC_rx_this_command_complete && (SFC_cmd_idx == 5);
+wire SFC_rx_this_command_complete = rx_valid_r && SFC_byte_sr[5];
 
 SFC_state_t SFC_state_next;
 always @(*) begin
     SFC_state_next = SFC_state;
     unique case (SFC_state)
         SFC_RX_COMMAND_SET: if (rx_valid_r) SFC_state_next = SFC_RX_FLASH_METHOD;
-        SFC_RX_FLASH_METHOD: if (rx_valid_r) SFC_state_next = SFC_RX_COMMAND;
-        SFC_RX_COMMAND: if (SFC_rx_all_commands_complete) SFC_state_next = SFC_COMPLETE;
+        SFC_RX_FLASH_METHOD: if (rx_valid_r) SFC_state_next = SFC_RX_FLASH_WE_PIN;
+        SFC_RX_FLASH_WE_PIN: if (rx_valid_r) SFC_state_next = SFC_RX_COMMAND_0;
+        SFC_RX_COMMAND_0: if (SFC_rx_this_command_complete) SFC_state_next = SFC_RX_COMMAND_1;
+        SFC_RX_COMMAND_1: if (SFC_rx_this_command_complete) SFC_state_next = SFC_RX_COMMAND_2;
+        SFC_RX_COMMAND_2: if (SFC_rx_this_command_complete) SFC_state_next = SFC_RX_COMMAND_3;
+        SFC_RX_COMMAND_3: if (SFC_rx_this_command_complete) SFC_state_next = SFC_RX_COMMAND_4;
+        SFC_RX_COMMAND_4: if (SFC_rx_this_command_complete) SFC_state_next = SFC_RX_COMMAND_5;
+        SFC_RX_COMMAND_5: if (SFC_rx_this_command_complete) SFC_state_next = SFC_COMPLETE;
         // SFC_COMPLETE: /* terminal until !enabled */ ;
         default: ;
     endcase
@@ -84,55 +94,144 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    if (SFC_state != SFC_RX_COMMAND) begin
-        SFC_byte_idx <= '0;
+    if (SFC_state == SFC_RX_FLASH_METHOD) begin
+        SFC_byte_sr <= { 5'b0, 1'b1 };
     end else if (rx_valid_r) begin
-        SFC_byte_idx <= SFC_byte_idx + 3'd1;
+        SFC_byte_sr <= { SFC_byte_sr[4:0], SFC_byte_sr[5] };
     end
 end
 
-always @(posedge clk) begin
-    if (SFC_state != SFC_RX_COMMAND) begin
-        SFC_cmd_idx <= '0;
-    end else if (SFC_rx_this_command_complete) begin
-        SFC_cmd_idx <= SFC_cmd_idx + 1'd1;
-    end
-end
-
-wire SFC_rx_command_byte = rx_valid_r && (SFC_state == SFC_RX_COMMAND);
 reg [15:0] SFC_address;
+reg [7:0] SFC_data;
+SFC_state_t SFC_store_state;
 
 always @(posedge clk) begin
-    if (SFC_rx_command_byte) begin
-        unique case (SFC_byte_idx)
-            // 0, 1: /* unused, AGB-only */ ;
-            2: SFC_address[15:8] <= rx_data_r;
-            3: SFC_address[7:0] <= rx_data_r;
-            // 4: /* unused, AGB-only */ ;
-            5: commands[SFC_cmd_idx] <= '{SFC_address, rx_data_r};
-            default: ;
-        endcase
-    end
+    SFC_store_state <= SFC_COMPLETE;
+    unique case (1'b1)
+        SFC_byte_sr[2]: SFC_address[15:8] <= rx_data_r;
+        SFC_byte_sr[3]: SFC_address[7:0] <= rx_data_r;
+        SFC_byte_sr[5]: begin
+            SFC_data <= rx_data_r;
+            if (rx_valid_r) SFC_store_state <= SFC_state;
+        end
+        default: ;
+    endcase
 end
-
+always @(posedge clk) begin
+    unique case (SFC_store_state)
+        SFC_RX_COMMAND_0: commands[0] <= '{ SFC_address, SFC_data };
+        SFC_RX_COMMAND_1: commands[1] <= '{ SFC_address, SFC_data };
+        SFC_RX_COMMAND_2: commands[2] <= '{ SFC_address, SFC_data };
+        SFC_RX_COMMAND_3: commands[3] <= '{ SFC_address, SFC_data };
+        SFC_RX_COMMAND_4: commands[4] <= '{ SFC_address, SFC_data };
+        SFC_RX_COMMAND_5: commands[5] <= '{ SFC_address, SFC_data };
+        default: ;
+    endcase
+end
 ///// </SET_FLASH_CMD> /////
 ///// <FLASH_PROGRAM> /////
 
 // We're occupying a BRAM slot, we might as well fill it (well, close enough) :)
-reg [7:0] FP_program [0:2047];
-reg [11:0] FP_rx_count;
-reg [15:0] FP_payload_address;
-reg [7:0] FP_payload_data;
-reg [11:0] FP_write_count;
-reg [11:0] FP_write_count_next;
-logic [2:0] FP_cmd_idx;
-logic [2:0] FP_cmd_idx_next;
-// up to 6 commands per data byte, up to 2048 data bytes, so up to 12288 commands
-reg [13:0] FP_pending_cart_commands;
+// 1-indexed rather than 0-indexed so we can index by 'remaining'
+reg [7:0] FP_program [1:2048];
 
-wire FP_rx_complete = (FP_rx_count == var_transfer_size);
-wire FP_exec_complete = (FP_write_count == FP_rx_count);
-wire FP_wait_cart_complete = (FP_pending_cart_commands == 0);
+///// <FLASH_PROGRAM:RX> /////
+reg [12:0] FP_RX_remaining;
+wire FP_RX_complete = (FP_RX_remaining == 0);
+
+always @(posedge clk) begin
+    if (!FLASH_PROGRAM_enable) begin
+        FP_RX_remaining <= var_transfer_size;
+    end else if (rx_valid_r) begin
+        FP_RX_remaining <= FP_RX_remaining - 13'd1;
+        FP_program[FP_RX_remaining] <= rx_data_r;
+    end
+end
+///// </FLASH_PROGRAM:RX> /////
+
+///// <FLASH_PROGRAM:EXEC> /////
+reg [12:0] FP_EXEC_remaining;
+reg [11:0] FP_EXEC_queue;
+wire FP_EXEC_complete = (FP_EXEC_remaining == 0);
+
+typedef enum {
+    FP_EXEC_IDLE,
+    FP_EXEC_FETCH,
+    FP_EXEC,
+    FP_EXEC_SUBMITTED
+} FP_EXEC_state_t;
+FP_EXEC_state_t FP_EXEC_state;
+
+reg [5:0] FP_EXEC_cmd_sr;
+always @(posedge clk) begin
+    unique case (FP_EXEC_state)
+        FP_EXEC_FETCH: FP_EXEC_cmd_sr <= 6'b000001;
+        FP_EXEC: if (cart_ready) FP_EXEC_cmd_sr <= { FP_EXEC_cmd_sr[4:0], FP_EXEC_cmd_sr[5] };
+        default : ;
+    endcase
+end
+
+always @(posedge clk) begin
+    if (!FLASH_PROGRAM_enable) begin
+        FP_EXEC_queue <= 13'd0;
+        FP_EXEC_remaining <= var_transfer_size;
+    end else if (rx_valid_r) begin
+        FP_EXEC_queue <= FP_EXEC_queue + 11'd1;
+    end else if (FP_EXEC_state == FP_EXEC_SUBMITTED) begin
+        FB_EXEC_queue <= FB_EXEC_queue - 11'd1;
+        FP_EXEC_remaining <= FP_EXEC_remaining - 11'd1;
+    end
+end
+
+FP_EXEC_state_t FP_EXEC_state_next;
+always @(*) begin
+    FP_EXEC_state_next = FP_EXEC_state;
+    unique case (FP_EXEC_state)
+        FP_EXEC_IDLE: if (FP_EXEC_queue > 0) FP_EXEC_state_next = FP_EXEC_FETCH_DATA;
+        FP_EXEC_FETCH: FP_EXEC_state_next = FP_EXEC;
+        FP_EXEC: if (cart_ready) FP_EXEC_state_next = FP_EXEC_SUBMITTED;
+        FP_EXEC_SUBMITTED:  FP_EXEC_state_next = FP_EXEC_IDLE;
+        default: ;
+    endcase
+end
+always @(posedge clk) begin
+    if (!FLASH_COMMAND_enable) FP_EXEC_state <= FP_EXEC_IDLE;
+    else FP_EXEC_state <= FP_EXEC_state_next;
+end
+
+reg [15:0] FP_EXEC_payload_address;
+reg [7:0] FP_EXEC_payload_data;
+command_t FP_EXEC_command;
+
+always @(posedge clk) begin
+    if (!FLASH_PROGRAM_enable) begin
+        FP_EXEC_payload_address <= var_address;
+    end else if (FP_EXEC_state == FP_EXEC_FETCH) begin
+        FP_EXEC_payload_data <= program[FP_EXEC_remaining];
+        for (int i = 0; i < 6; i = i + 1) begin
+            if (FB_EXEC_cmd_sr[i]) FP_EXEC_command <= command[i];
+        end
+    end else if (FP_EXEC_state == FP_EXEC_SUBMITTED) begin
+        FP_EXEC_payload_address <= FP_EXEC_payload_address + 16'd1;
+    end
+end
+
+reg FP_EXEC_have_payload;
+always @(posedge clk) begin
+    if (state != FP_EXEC) begin
+        FP_EXEC_have_payload <= 1'b0;
+    end
+    if (cart_ready && !have_payload) begin
+        // TODO
+    end
+end
+
+///// </FLASH_PROGRAM:EXEC> /////
+
+// Our cart FIFO is 512-deep, so... we're can't go further :D
+reg [9:0] FP_pending_cart_commands;
+logic [2:0] FP_cmd_idx;
+reg [15:0] FP_address;
 
 typedef enum {
     FP_RX_AND_EXEC,
@@ -143,113 +242,6 @@ typedef enum {
 FP_state_t FP_state;
 always @(posedge clk) FLASH_PROGRAM_complete <= (FP_state == FP_COMPLETE);
 
-FP_state_t FP_state_next;
-always @(*) begin
-    FP_state_next = FP_state;
-    unique case (FP_state)
-        FP_RX_AND_EXEC: if (FP_rx_complete) FP_state_next = FP_EXEC_ONLY;
-        FP_EXEC_ONLY: if (FP_exec_complete) FP_state_next = FP_WAIT_CART_COMPLETE;
-        FP_WAIT_CART_COMPLETE: if (FP_wait_cart_complete) FP_state_next = FP_COMPLETE;
-        // FP_COMPLETE: /* terminal until !enable */ ;
-        default: ;
-    endcase
-end
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) begin
-        FP_state <= FP_RX_AND_EXEC;
-    end else begin
-        FP_state <= FP_state_next;
-    end
-end
-
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) begin
-        FP_rx_count <= 1'd0;
-    end else if (rx_valid_r) begin
-        FP_rx_count <= FP_rx_count + 1'd1;
-    end
-end
-
-wire FP_rx = (FP_state == FP_RX_AND_EXEC) && rx_valid_r;
-always @(posedge clk) begin
-    if (FP_rx) begin
-        FP_program[FP_rx_count] <= rx_data_r;
-    end
-end
-
-command_t FP_cmd_it;
-assign FP_cmd_it = commands[FP_cmd_idx];
-wire FP_cmd_is_payload = (FP_cmd_it == '{default: 0});
-
-wire FP_exec = cart_ready && !FP_exec_complete;
-logic [15:0] FP_cart_address_next;
-logic [7:0]  FP_cart_data_next;
-always @(*) begin
-    FP_cart_address_next = commands[FP_cmd_idx].address;
-    FP_cart_data_next = commands[FP_cmd_idx].data;
-    if (FP_cmd_is_payload) begin
-        FP_cart_address_next = FP_payload_address;
-        FP_cart_data_next = FP_payload_data;
-    end
-end
-always @(posedge clk) begin
-    cart_req_valid <= 1'b0;
-    cart_req_address <= 16'd0;
-    cart_req_data <= 8'd0;
-    cart_req_wait_for_status <= 1'b0;
-    if (FP_exec) begin
-        cart_req_valid <= 1'd1;
-        cart_req_address <= FP_cart_address_next;
-        cart_req_data <= FP_cart_data_next;
-        cart_req_wait_for_status <= FP_cmd_is_payload;
-    end
-end
-
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) begin
-        FP_pending_cart_commands <= '0;
-    end else begin
-        unique case ({FP_exec, cart_complete})
-            2'b11, 2'b00: /* no change */ ;
-            2'b10: FP_pending_cart_commands <= FP_pending_cart_commands + 14'd1;
-            2'b01: FP_pending_cart_commands <= FP_pending_cart_commands - 14'd1;
-        endcase
-    end
-end
-
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) begin
-        FP_payload_address <= var_address;
-    end else if (FP_exec && FP_cmd_is_payload) begin
-        FP_payload_address <= FP_payload_address + 16'd1;
-    end
-end
-
-// In theory this is a cycle late
-// In practice, we never need it for the first command
-always @(posedge clk) FP_payload_data <= FP_program[FP_write_count];
-
-always @(*) begin
-    FP_cmd_idx_next = FP_cmd_idx;
-    if (FP_write_count == FP_rx_count) begin
-        FP_cmd_idx_next = 0;
-    end else if (FP_exec) begin
-        FP_cmd_idx_next = FP_cmd_is_payload ? '0 : (FP_cmd_idx + 3'd1);
-    end
-end
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) FP_cmd_idx <= '0;
-    else FP_cmd_idx <= FP_cmd_idx_next;
-end
-
-always @(*) begin
-    FP_write_count_next = FP_write_count;
-    if (FP_exec && FP_cmd_is_payload) FP_write_count_next = FP_write_count + 12'd1;
-end
-always @(posedge clk) begin
-    if (!FLASH_PROGRAM_enable) FP_write_count <= 1'd0;
-    else FP_write_count <= FP_write_count_next;
-end
 ///// </FLASH_PROGRAM> /////
 
 endmodule
