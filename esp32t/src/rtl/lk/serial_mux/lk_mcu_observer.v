a06a8eb6306af6162848c8ff11ef29f750ef3161
Fred Emmott <git@fredemmott.com>
2026-06-03 19:30:27 -0500

    MCU observer: ignore MCU packet bodies

--- esp32t/src/rtl/lk/serial_mux/lk_mcu_observer.v
+++ esp32t/src/rtl/lk/serial_mux/lk_mcu_observer.v
@@ -41,25 +41,44 @@ always @(posedge clk) begin
     end
 end
 
+// MCU V1: (header, addr, payload0, payload1)
+//
+// If we see the v1 header, we ignore the next 3 bytes
+//
+// MCU V2: (header, addr, len, payload[0..(len - 1)],crc)
+//
+// len is at most 14
+// so, once we've seen the len, we have *at most* 15 bytes to ignore: 14 bytes of payload, then the CRC
+// 15 fits in 4 bits, so:
+reg [3:0] ignore_count;
+
 typedef enum {
     S_DEFAULT,
-/* TODO
-    // pass through up to 3 bytes (len, payload0, payload1] to the MCU over UART
-    // Cancel and reset to S_DEFAULT if the header byte is seen again
-    S_MCU_V1_RX,
-    // read the address byte from a v2 packet (header, addr, len, payload[...], crc byte)
+    S_LK_SERIAL_ID,
+    S_LK,
+    // read the address byte from a v2 packet
     S_MCU_V2_RX_ADDR,
     // read the length byte from a v2 packet
     S_MCU_V2_RX_LEN,
-    // pass through a specific number of bytes to the UART, then revert to DEFAULT.
-    // Used for V2 packets once the length has been counted
-    S_MCU_RX_COUNTED,
-*/
-    S_LK_SERIAL_ID,
-    S_LK
+    // Skip packets based on ignore_count
+    S_MCU_RX_COUNTED
 } state_t;
 
 state_t state = S_DEFAULT;
+
+always @(posedge clk) begin
+    unique case (state)
+        // Use the MCU V1 counter here, as:
+        // - it only gets used if we go into S_MCU_RX_COUNTED
+        // - for V2, we go into S_MCU_V2_RX_LEN first
+        S_DEFAULT: ignore_count <= 4'd3;
+        // Don't bother with rx_new_byte: it will be set on the last cycle we spend here.
+        // + 1 for CRC
+        S_MCU_V2_RX_LEN: ignore_count <= rx_data + 1;
+        S_MCU_RX_COUNTED: if (rx_new_byte) ignore_count <= ignore_count - 4'd1;
+        default: ;
+    endcase
+end
 
 always @(*) begin
     unique case (state)
@@ -81,8 +100,13 @@ always @(*) begin
                 if (rx_new_byte) begin
                     if ((rx_data_d == 8'h55) && (rx_data == 8'hAA)) next_state = S_LK_SERIAL_ID;
                     else if ((rx_data_d == "L") && (rx_data == "K")) next_state = S_LK;
+                    else if (rx_data == 8'h8A) next_state = S_MCU_RX_COUNTED; // MCU V1
+                    else if (rx_data == 8'h8F) next_state = S_MCU_V2_RX_ADDR;
                 end
             end
+            S_MCU_V2_RX_ADDR: if (rx_new_byte) next_state = S_MCU_V2_RX_LEN;
+            S_MCU_V2_RX_LEN: if (rx_new_byte) next_state = S_MCU_RX_COUNTED;
+            S_MCU_RX_COUNTED: if (rx_new_byte && (ignore_count == 4'd1)) next_state = S_DEFAULT;
             S_LK_SERIAL_ID, S_LK: /* terminal until reset */ ;
             default: next_state = S_DEFAULT;
         endcase
