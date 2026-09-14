f1ffe01b820a244277eda406b8665e4331207b5f
Fred Emmott <git@fredemmott.com>
2026-09-14 05:06:44 -0500

    Add length field to serial ID response

--- esp32t/src/rtl/lk/serial_mux/lk_serial_id.v
+++ esp32t/src/rtl/lk/serial_mux/lk_serial_id.v
@@ -7,6 +7,10 @@ module lk_serial_id_t(
 );
 
 localparam ROM_BLOB = {
+    // Size of this response block, overwritten in initial block
+    // This is intended to allow chaining with other modifications or IDs
+    16'd0,
+    // Identify this block
     "fredemmott/CartIO", 8'h00,
 
     // Our version timestamp - BCD
@@ -31,6 +35,8 @@ initial begin
     for (i = 0; i < ROM_LEN; i = i + 1) begin
         rom[i] = ROM_BLOB[(ROM_LEN - 1 - i)*8 +: 8];
     end
+    rom[0] = ROM_LEN >> 8;
+    rom[1] = ROM_LEN & 8'hFF;
 end
 
 reg [ROM_ADDR_WIDTH-1:0] idx;
